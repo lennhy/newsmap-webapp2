3f61110ff3dfc4c52e688a139609a0f7100a0768
Lenn Hypolite <lenntriniman@hotmail.com>
2016-11-03 19:11:47 +0000

    Added credits text in line 23 of show page for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
   def total_reader_credits
     credits.count
   end
-  
+
   # -- create user model when signin with facebook
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
