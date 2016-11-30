b2234b37f2b07f6ce54fd02e6ffd78f293962fe9
lenn hypolite <lennhydesign@gmail.com>
2016-11-29 21:01:23 -0500

    broke up the larger function with the ajax request and the article toggle function into two smaller functions. One for the ajax request and the other for the toggle

--- app/assets/javascripts/custom_code.js
+++ app/assets/javascripts/custom_code.js
@@ -1,33 +1,36 @@
 // Shorthand for $( document ).ready()
 $(function() {
   $(".js-read-more").on('click', function() {
-    var id = $(this).data("id");
-     expand(id);
+    let id = $(this).data("id");
+     showArticle(id);
    });
 });
 
 // ajax request to show article content on index page
-function expand(id){
+function showArticle(id){
   $.get("/articles/" + id + "/body", function(data){
-      var addText = $("#body-" + id);
-      // first make sure the data obj from the ajax reuest is received
-      addText.text(data);
-      // 
-      if($(`.js-read-more[data-id=${id}]`).html() === "Read More"){
-        addText.show();
-      $(`.js-read-more[data-id=${id}]`).html("Read Less");
-      }
-      else{
-        addText.hide();
-        $(`.js-read-more[data-id=${id}]`).html("Read More");
-
-      }
-
-
+    toggleArticle(id, data);
   }).done(function(data){
     console.log("request completed");
-
   }).fail(function(jqXHR, textStatus, errorThrown){
     alert(errorThrown);
   });
+}
+
+function toggleArticle(id, data){
+  let addText = $("#body-" + id);
+  let button = $(".js-read-more");
+  // first make sure the data obj from the ajax reuest is received
+  // if text is not visible then show it on click
+  addText.text(data);
+  // then check if the body of the text is not visible
+  if(button.html() === "Read More"){
+    addText.show();
+    button.html("Read Less");
+  }
+  // if text is visible then hide it on click
+  else{
+    addText.hide();
+    $(".js-read-more").html("Read More");
+  }
 }
