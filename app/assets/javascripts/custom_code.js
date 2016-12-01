5b7105fd6b1ce366768367bd5de832828df9be37
lenn hypolite <lennhydesign@gmail.com>
2016-12-01 12:55:13 -0500

    split the functionality of the ajax request for the user show page into two seperate functions to allow for toggle functionality

--- app/assets/javascripts/custom_code.js
+++ app/assets/javascripts/custom_code.js
@@ -1,7 +1,7 @@
 //--- Shorthand for $( document ).ready()
 $(function() {
   getAllArticles();
-  loadArticleOnClick()
+  loadArticleOnClick();
 });
 
 function loadArticleOnClick(){
@@ -11,14 +11,12 @@ function loadArticleOnClick(){
    });
  }
 
-
-//-- Ajax request to show article content on index page
+// Request to show article content on index page
 function showArticle(id){
   $.get("/articles/" + id + ".json", function(articles){
     let content = articles["content"];
     toggleArticle(id, content);
   })
-
   .done(function(content){
     // console.log("request completed");
   })
@@ -27,52 +25,66 @@ function showArticle(id){
   });
 }
 
+// toggle article in index page
 function toggleArticle(id, content){
   let addText = $("#body-" + id);
   let button = $(".js-read-more");
   let article_details = $("#article-details");
-
   addText.html("<p>" + content + "</p>");
-  //-- then check if the body of the text is not visible
+
+  // toggle action
   if(button.html() === "Read More"){
     addText.show();
     button.html("Read Less");
   }
-  //-- if text is visible then hide it on click
+  // if text is visible then hide it on click
   else{
     addText.hide();
     $(".js-read-more").html("Read More");
   }
 }
 
-// -- Ajax request for loadinf all article titles, creditors, authors etc on index page
+// Ajax request for loading all article titles, creditors, authors etc on index page
 function getAllArticles(id) {
   $("#articlesLink").on('click', function() {
-    var id = $(this).data("id");
-    $.get("/users/" + id + ".json", function(userObj){
-      //  save each part of the user Object that you want to spit out on the DOM
-      var articles = userObj.articles;
-      var credits = articles.map(function(articleObj) {
-        if(articleObj.credits.length !== 0){
-           return articleObj.credits
-        }else{
-          return "There are no creditors for this article";
-        }
-      });
-      // spit out the save variables in the DOM
-      $.each(articles, function(i, article){
-        $(".index-container ").prepend("<p><a href='/users/3/articles/3'>" + article["title"] +"<a>"  + " Credits: " + article.total_credits + "</p>" );
-      });
-  })
-  .done(function(content){
-    console.log("request completed");
-  })
-  .fail(function(jqXHR, textStatus, errorThrown){
-    console.log(errorThrown);
-  });
-});
-}
+    let id = $(this).data("id");
 
+    })
+    .done(function(content){
+      console.log("request completed");
+    })
+    .fail(function(jqXHR, textStatus, errorThrown){
+      console.log(errorThrown);
+    });
+  });
+}
+function toggleUserArticles(id, content){
+  $.get("/users/" + id + ".json", function(userObj){
+    //  save each part of the user Object that you want to spit out on the DOM
+    let articles = userObj.articles;
+
+    let credits = articles.map(function(articleObj) {
+      if(articleObj.credits.length !== 0){
+         return articleObj.credits
+      }else{
+        return "There are no creditors for this article";
+      }
+    });
+    // spit out the save variables in the
+    $.each(articles, function(i, article){
+      $(".index-container ").prepend("<p><a href='/users/3/articles/3'>" + article["title"] +"<a>"  + " Credits: " + article.total_credits + "<p>" + article["content"] + "</p>" + "</p>" );
+    });
+    // toggle action
+    if(button.html() === "Read More"){
+      addText.show();
+      button.html("Read Less");
+    }
+    // if text is visible then hide it on click
+    else{
+      addText.hide();
+      $(".js-read-more").html("Read More");
+    }
+}
 // '<a href="/users/3/articles/3">article["title"]</a>'
 // ============================= FORM =============================
 // $(function () {
