b29fd1096f50f035bcfbec83fa1d7c5daadb51da
lennhy <lennhydesign@gmail.com>
2017-04-29 03:06:42 -0400

    Begin to customize infoWindow

--- app/assets/javascripts/custom_code.js
+++ app/assets/javascripts/custom_code.js
@@ -32,8 +32,8 @@ var userObjGlobalVar;
 //     userArticles.renderUserArticles();
 //   });
 // }
-//
-// // Load article content
+
+// Load article content
 // function loadArticleDetails(){
 //   $(".article-info").each(function (i, element){
 //     let id = $(this).data("id");
@@ -49,7 +49,7 @@ var userObjGlobalVar;
 //     });
 //   });
 // }
-//
+
 // function loadArticle(id){
 //   $.get("/articles/" + id + ".json", function(articles){
 //      let content = articles["content"];
@@ -63,7 +63,7 @@ var userObjGlobalVar;
 //     });
 // }
 //
-// //  ---------------------------------------REQUEST ALL CURRENT USER ARTICLES VIA GET REQUEST
+// //  --------------------------------------- REQUEST ALL CURRENT USER ARTICLES VIA GET REQUEST
 //
 // function loadAllCurrentlUserArticles(userId){
 //   $.get("/users/" + userId + ".json", function(userObj){
@@ -77,36 +77,36 @@ var userObjGlobalVar;
 //
 // // -------------------------------------- FORMS: ADDING A CREDIT TO ARTICLE
 //
-// function submitForm(){
-//   $('form.new_credit').submit(function(event) {
-//     event.preventDefault();
-//
-//     let artId = $(this).find("span").data("id");
-//     let values = $(this).serialize();
-//     let crediting = $.post('/credits', values);
-//
-//     crediting.done(function(creditObj, textStatus, jqXHR ){
-//
-//       $('input[type="submit"]').prop("disabled", false);
-//       loadArticleDetails();
-//
-//       let articleId = $("#notify-"+artId);
-//
-//       articleId.css("color", "red");
-//
-//       if(jqXHR.status === 200){
-//         if((articleId).html("")){
-//           articleId.prepend("<p>You have already credited this article</p>").show().fadeOut(5000);
-//         }
-//       }
-//       else if(jqXHR.status === 201){
-//         if((articleId).html("")){
-//          articleId.prepend("<p>You have now added a new credit to the article</p>").show().fadeOut(5000);
-//         }
-//       }
-//     });
-//   });
-// }
+function submitForm(){
+  $('form.new_credit').submit(function(event) {
+    event.preventDefault();
+
+    let artId = $(this).find("span").data("id");
+    let values = $(this).serialize();
+    let crediting = $.post('/credits', values);
+
+    crediting.done(function(creditObj, textStatus, jqXHR ){
+
+      $('input[type="submit"]').prop("disabled", false);
+      loadArticleDetails();
+
+      let articleId = $("#notify-"+artId);
+
+      articleId.css("color", "red");
+
+      if(jqXHR.status === 200){
+        if((articleId).html("")){
+          articleId.prepend("<p>You have already credited this article</p>").show().fadeOut(5000);
+        }
+      }
+      else if(jqXHR.status === 201){
+        if((articleId).html("")){
+         articleId.prepend("<p>You have now added a new credit to the article</p>").show().fadeOut(5000);
+        }
+      }
+    });
+  });
+}
 //
 // //  ---------------------------------- OBJECT CONSTRUCTOR FUNCTIONS
 //
@@ -167,73 +167,123 @@ var userObjGlobalVar;
 
 // ----------------------------------------------- GOOGLE MAPS API
 
-// Global Varaibles
+// --------------------------------- Global Varaibles
+
 var geocoder;
 var map;
 
-// Initialize map
+
+
+// --------------------------------- Initialize map
+
 function initMap() {
-  // Setup initial point of view on Map
+
+
+
+  // ------------------------------- Starting view on Map
+
   geocoder = new google.maps.Geocoder();
   var latlng = new google.maps.LatLng(-34.397, 150.644);
   var mapOptions = {
-    zoom: 8,
-    center: latlng
+    zoom: 3,
+    center: latlng,
+    mapTypeControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      position: google.maps.ControlPosition.TOP_CENTER
+    },
+    zoomControl: true,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.LEFT_CENTER
+    },
+    scaleControl: true,
+    streetViewControl: true,
+    streetViewControlOptions: {
+      position: google.maps.ControlPosition.LEFT_TOP
+    },
+    fullscreenControl: true
   }
   map = new google.maps.Map(document.getElementById('map'), mapOptions);
   codeAddress(map);
 }
 
-// Set all markers by address enterred
+
+
+// ----------------------------- Set all markers by address enterred
+
 function codeAddress(map){
   var addresses=[];
-  articlesArr =[];
+  var articlesArr =[];
+  var marker;
+
   $.get("/articles.json", function(){
   }).done(function(articles){
-    articlesArr.push(articles.map(function(art, i){
-      return art.title + ": "+ art.content;
-    }));
+    console.log(articles);
+    // let content = articles[i]["content"];
+    // let total_credits = (articles[i]["total_credits"]);
+    // let author = (articles[i]["user"]["name"]);
+    // articlesArr.push(articles.map(function(art, i){
+    //   return "Arthor: "+author+"</li>" + "<li>Total Credits: "+total_credits+"<li class='city'>"+articles[i]["address"]["city"]+", "+articles[i]["address"]["country"]["title"]+"</li>";
+    // }));
     addresses.push(articles.map(function(art, i){
       return art.address.city + ", "+ art.address.country.title;
     }));
-    returnAddresses(addresses, articlesArr);
+    returnAddresses(addresses, articles);
   });
 
-  var marker;
 
-  function returnAddresses(addressArray, articlesArray){
-    console.log(articlesArray[0][0]);
-    // Create Marker
+
+  // ---------------------------- Create Marker with address
+
+  function returnAddresses(addressArray, articles){
+    // console.log(articlesArray[0][0]);
     var infowindow = new google.maps.InfoWindow({
-      content: ''
+      content: '',
+      minWidth: 300,
+      backgroundClassName: 'phoney',
     });
     for(let i=0; i < addressArray[0].length; i++){
       geocoder.geocode(
       {
        'address': addressArray[0][i]
       },
-      // check if promise was returned successfully
+
+
+
+      // ------------------------- Check if promise was returned successfully
+
       function(results, status) {
         if (status == 'OK') {
           map.setCenter(results[0].geometry.location);
             // Set marker
             var marker = new google.maps.Marker({
             map: map,
-             bounds: true,
             position: results[0].geometry.location,
             id: i
           });
 
 
 
-          marker.addListener('click', function(){
-            console.log("eye");
-            infowindow.setContent(articlesArray[0][i]);
-            infowindow.setPosition(results[0].geometry.location);
+        // ------------------------- Execute info window
 
-              infowindow.open(map, marker);
+        let content = articles[i]["content"];
+        let total_credits = (articles[i]["total_credits"]);
+        let author = (articles[i]["user"]["name"]);
+        articlesArr.push(articles.map(function(art, i){
+           return "Arthor: "+author+"</li>" +
+                        "<li>Total Credits: "+
+                        total_credits+
+                        "<li class='city'>"+
+                        articles[i]["address"]["city"]+
+                        ", "+
+                        articles[i]["address"]["country"]["title"]+
+                        "</li>"+
+                        articles[i].title +
+                        ": "+
+                         articles[i].content
+        }));
 
-          });
+        openInfoWindow(results, marker, infowindow, articlesArr, i);
 
         }else{
           alert('Geocode was not successful for the following reason: ' + status);
@@ -241,4 +291,16 @@ function codeAddress(map){
       });
     }
   }
+}
+
+
+
+// ------------------------- Create info window
+
+function openInfoWindow(results, marker, infowindow, articlesArray, index){
+  marker.addListener('click', function(){
+    infowindow.setContent(articlesArray[0][index]);
+    infowindow.setPosition(results[0].geometry.location);
+    infowindow.open(map, marker);
+  });
 }
